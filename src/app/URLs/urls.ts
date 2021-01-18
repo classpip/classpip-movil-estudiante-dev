57109a207b761604c6c33f91dfd29ae757191953
Izan Pérez Cosano <izanperez@yandex.com>
2021-01-18 18:05:59 +0100

    localhost environment

--- src/app/URLs/urls.ts
+++ src/app/URLs/urls.ts
@@ -1,5 +1,5 @@
-export const host = 'http://147.83.118.92:';
-// export const host = 'localhost';
+// export const host = 'http://147.83.118.92:';
+export const host = 'http://localhost:';
 
 export const ImagenesColeccion = host + '3000/api/imagenes/imagenColeccion/download/';
 export const ImagenesCromo = host + '3000/api/imagenes/imagenCromo/download/';
@@ -8,6 +8,5 @@ export const AudiosAvatares = host + '3000/api/imagenes/AudiosAvatares/download/
 export const ImagenesPerfil = host + '3000/api/imagenes/ImagenesPerfil/download/';
 export const ImagenesPregunta = host + '3000/api/imagenes/ImagenesPreguntas/download/';
 export const LogosEquipos = host + '3000/api/imagenes/LogosEquipos/download/';
-export const ImagenesPregunta = host + '3000/api/imagenes/ImagenesPreguntas/download/';
 
 export const Servidor = host + '8080';
