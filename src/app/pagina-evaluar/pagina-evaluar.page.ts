a22db97246666f1673d43c7e7a3adfc0f3da6303
Izan Pérez Cosano <izanperez@yandex.com>
2021-01-21 14:59:22 +0100

    tick list and array response

--- src/app/pagina-evaluar/pagina-evaluar.page.ts
+++ src/app/pagina-evaluar/pagina-evaluar.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {PeticionesAPIService, SesionService} from '../servicios';
-import {Alumno, Equipo, Rubrica} from '../clases';
+import {Alumno, Criterio, Equipo, Rubrica} from '../clases';
 import {JuegoDeEvaluacion} from '../clases/JuegoDeEvaluacion';
 
 @Component({
@@ -20,6 +20,7 @@ export class PaginaEvaluarPage implements OnInit {
   // ?juegoEquiposDeEvaluacion
   equipos: Equipo[];
   rubrica: Rubrica;
+  respuestaEvaluacion: Array<any>;
 
   constructor(
       private route: ActivatedRoute,
@@ -27,6 +28,26 @@ export class PaginaEvaluarPage implements OnInit {
       private sesion: SesionService
   ) {
     this.rutaId = parseInt(this.route.snapshot.paramMap.get('id'), 10);
+  }
+
+  ngOnInit() {
+    this.juego = this.sesion.DameJuegoEvaluacion();
+    this.miAlumno = this.sesion.DameAlumno();
+    this.miEquipo = this.sesion.DameEquipo();
+    this.peticionesAPI.DameRubrica(this.juego.rubricaId).subscribe((res: Rubrica) => {
+      this.rubrica = res;
+      this.respuestaEvaluacion = new Array<any>(this.rubrica.Criterios.length);
+      this.rubrica.Criterios.forEach((item, index) => {
+        this.respuestaEvaluacion[index] = new Array<boolean>(this.rubrica.Criterios[index].Elementos.length).fill(false);
+      });
+      this.respuestaEvaluacion.push('');
+      console.log(this.respuestaEvaluacion);
+    });
+    if (this.juego.Modo === 'Individual') {
+      this.alumnos = this.sesion.DameAlumnos();
+    } else if (this.juego.Modo === 'Equipos') {
+      this.equipos = this.sesion.DameEquipos();
+    }
   }
 
   DameNombreEvaluado(): string {
@@ -46,20 +67,6 @@ export class PaginaEvaluarPage implements OnInit {
     } else if (this.juego.Modo === 'Equipos' && typeof this.equipos !== 'undefined') {
       const equipo: Equipo = this.equipos.find(item => item.id === this.rutaId);
       return equipo.FotoEquipo;
-    }
-  }
-
-  ngOnInit() {
-    this.juego = this.sesion.DameJuegoEvaluacion();
-    this.miAlumno = this.sesion.DameAlumno();
-    this.miEquipo = this.sesion.DameEquipo();
-    this.peticionesAPI.DameRubrica(this.juego.rubricaId).subscribe((res: Rubrica) => {
-      this.rubrica = res;
-    });
-    if (this.juego.Modo === 'Individual') {
-      this.alumnos = this.sesion.DameAlumnos();
-    } else if (this.juego.Modo === 'Equipos') {
-      this.equipos = this.sesion.DameEquipos();
     }
   }
 
