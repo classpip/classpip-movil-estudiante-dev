7d0bc419e88bf335219c5ec4bf3e7c4fd0e64840
Miguel Valero <miguel.valero@upc.edu>
2021-03-19 22:58:25 +0100

    sin boton juego rapido

--- src/app/URLs/urls.ts
+++ src/app/URLs/urls.ts
@@ -1,5 +1,5 @@
-export const host = 'http://147.83.118.92:';
-//export const host = 'http://147.83.249.79:';
+//export const host = 'http://147.83.118.92:';
+export const host = 'http://147.83.249.79:';
 //export const host = 'http://classpip.upc.edu:';
 //export const host = 'http://localhost:';
 
