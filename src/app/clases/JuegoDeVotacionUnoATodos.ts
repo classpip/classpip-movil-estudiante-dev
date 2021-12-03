4aba8fcc80d89321683b821f9cd200d4a8e6b5c1
miguelvalero <miguel.valero@upc.edu>
2021-12-03 13:21:17 +0100

    votacion individual juego equipo

--- src/app/clases/JuegoDeVotacionUnoATodos.ts
+++ src/app/clases/JuegoDeVotacionUnoATodos.ts
@@ -5,22 +5,26 @@ export class JuegoDeVotacionUnoATodos {
   NombreJuego: string;
   Modo: string;
   ModoReparto: string;
+  PermitirAutovotacion: boolean;
   JuegoActivo: boolean;
   JuegoTerminado: boolean;
   Puntos: number[];
+  VotanEquipos: boolean; // en los juegos de equipo pueden votar equipos o alumnos
 
 
-  constructor(Tipo?: string, Modo?: string, ModoReparto?: string,  JuegoActivo?: boolean,
+  constructor(Tipo?: string, Modo?: string, ModoReparto?: string,  PermitirAutovotacion?: boolean, JuegoActivo?: boolean,
               Puntos?: number[], NombreJuego?: string,
-              JuegoTerminado?: boolean, grupoId?: number) {
+              JuegoTerminado?: boolean, grupoId?: number, VotanEquipos?: boolean) {
 
     this.Tipo = Tipo;
     this.Modo = Modo;
     this.ModoReparto = ModoReparto;
+    this.PermitirAutovotacion = PermitirAutovotacion;
     this.JuegoActivo = JuegoActivo;
     this.Puntos = Puntos;
     this.NombreJuego = NombreJuego;
     this.JuegoTerminado = JuegoTerminado;
     this.grupoId = grupoId;
+    this.VotanEquipos = VotanEquipos;
   }
 }
